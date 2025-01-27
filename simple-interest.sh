6d0494b84dfe04d6046a0470f25eea3f5e9a363e
Mohan79932 <89894088+Mohan79932@users.noreply.github.com>
2025-01-27 11:16:15 -0800

    simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to calculate Simple Interest
+
+# Function to calculate simple interest
+calculate_simple_interest() {
+    local principal=$1
+    local rate=$2
+    local time=$3
+
+    # Calculate simple inter
